5f61a8eeba5210cf8e9747ad031787ef029e6a65
wangxu <214047695@qq.com>
2019-12-23 11:47:35 +0800

    首页添加

---
+++ src/main/java/com/wangxu/bootweb/controller/BookContoller.java
@@ -0,0 +1,27 @@
+package com.wangxu.bootweb.controller;
+
+import com.wangxu.bootweb.domain.BookCateInfo;
+import com.wangxu.bootweb.service.RESTService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * 图书控制器，用来显示图书页面的
+ */
+@Controller
+public class BookContoller {
+
+    @Autowired
+    private RESTService restService;
+
+    @RequestMapping(value = "/main")
+    public String main(Model model){
+        List<BookCateInfo> allCateList = restService.findAllCateList();
+        model.addAttribute("cateList",allCateList);
+        return "main";
+    }
+}
